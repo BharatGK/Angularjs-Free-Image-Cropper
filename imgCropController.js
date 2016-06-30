4a998c4bdcdbd71069bacf2be88f77f469200954
Bharat Kadam <bharat.kadam@mindbowser.com>
2016-06-30 18:13:41 +0530

    commit: - Angularjs free image cropper initial commit

---
+++ imgCropController.js
@@ -0,0 +1,23 @@
+var imageCropper=angular.module('imageCropper',['ngImgCrop']);
+imageCropper.controller("ImageCropperCtrl",[ '$scope','$rootScope',function($scope,$rootScope) 
+{    
+      
+						
+					          $scope.myImage='';
+						      $rootScope.myCroppedImage='';
+							  $scope.type='square';
+                            
+    
+						    //Got
+					        var handleFileSelect=function(evt) {
+					          var file=evt.currentTarget.files[0];
+					          var reader = new FileReader();
+					          reader.onload = function (evt) {
+					            $scope.$apply(function($scope){
+					              $scope.myImage=evt.target.result;
+					            });
+					          };
+					          reader.readAsDataURL(file);
+					        };
+					        angular.element(document.querySelector('#inputRectangleFileId')).on('change',handleFileSelect);    
+}]);
